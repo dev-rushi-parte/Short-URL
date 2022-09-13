267e495e6bc2b8497cd3c5cfcdfe3e88e2d8f27e
rushi2784 <96285307+rushi2784@users.noreply.github.com>
2022-09-13 13:08:16 +0530

    db ready

--- backend/index.js
+++ backend/index.js
@@ -1,11 +1,22 @@
-const express=require("express");
-const cors=require("cors");
+const express = require("express");
+const cors = require("cors");
 require("dotenv").config();
 
-const app=express();
+const app = express();
 app.use(express.json());
 
 app.use(cors());
 
 
-const connection=require("./config/config");
+const connection = require("./config/config");
+
+
+app.listen(process.env.PORT, async () => {
+    try {
+        await connection;
+        console.log(`connect to db on port ${process.env.PORT}`)
+    }
+    catch (err) {
+        console.log(`err in connecting to db ${err}`)
+    }
+})
